448b21ac92d8a1680330beefeb3cc4fc59b8f6a5
Suleyman <suleymanbasbug@hotmail.com>
2023-04-09 17:31:02 +0300

    added font at global

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,10 @@
+import { Poppins } from "next/font/google";
 import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "SBasbug",
@@ -9,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>
+        <main className="container">{children}</main>
+      </body>
     </html>
   );
 }
